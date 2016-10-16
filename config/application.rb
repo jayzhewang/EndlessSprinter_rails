76f05ba3af16ebec9807dffbce3728e9c6d529f7
Zhe Wang <akina.dwnhl@gmail.com>
2016-10-16 16:23:20 -0700

    added rack cors

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,11 @@ module EndlessSprinterScore
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post]
+      end
+    end
   end
 end
